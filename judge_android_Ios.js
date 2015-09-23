c7188e0d3a510de3ece2a5f3ae29b10875c0284a
alihanniba <alihanniba@gmail.com>
2015-09-23 18:22:06 +0800

    wechat development

---
+++ judge_android_Ios.js
@@ -0,0 +1,9 @@
+//Ios 與andriod判斷
+var userAgent = window.navigator.userAgent.toLowerCase();
+	if (userAgent.match(/iphone/i) || userAgent.match(/ipad/i) || userAgent.match(/ipod/i)) {
+             		
+	} else if (userAgent.match(/android/i)) {
+	             
+	} else {
+	             
+	}
